615c83734a0c0407e748ab40a70bab47d9131190
47gfx <117771075+4700gfx@users.noreply.github.com>
2024-07-31 02:22:06 -0400

    Adding No Footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import MovieList from './components/MovieList';
-import Footer from './components/Footer';
+
 
 
 function App() {
